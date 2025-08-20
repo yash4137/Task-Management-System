a10149c960f808981824c084e7210c1d9f3196e7
yash4137 <23cs011@charusat.edu.in>
2025-08-20 20:26:41 +0530

    1.0

--- frontend/task-manager/src/pages/Admin/Dashboard.jsx
+++ frontend/task-manager/src/pages/Admin/Dashboard.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUserAuth } from '../../hooks/useUserAuth';
 import { useContext } from 'react';
 import { UserContext } from '../../context/userContext';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
 import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import moment from 'moment';
+import { addThousandsSeparator } from '../../utils/helper';
+import InfoCard from '../../components/Cards/InfoCard';
+import { LuArrowRight } from 'react-icons/lu';
+import TaskListTable from '../../components/TaskListTable';
 
 const Dashboard = () => {
   useUserAuth();
@@ -16,8 +23,90 @@ const Dashboard = () => {
   const [pieChartData, setPieChartData] = useState([]);
   const [barChartData, setBarChartData] = useState([]);
 
+  const getDashboardData = async () => {
+    try{
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA
+      );
+      if(response.data){
+        setDashboardData(response.data);
+      }
+    } catch(error) {
+      console.log("Error fetching users:", error);
+    }
+  };
+
+  const onSeeMore = () => {
+    navigate('/admin/tasks')
+  };
+
+  useEffect(() => {
+    getDashboardData();
+
+    return () => {};
+  }, []);
+
   return (
-    <DashboardLayout activeMenu="Dashboard">Dashboard</DashboardLayout>
+    <DashboardLayout activeMenu="Dashboard">
+      <div className='card my-5'>
+        <div>
+          <div className='col-span-3'>
+            <h2 className='text-xl md:text-2xl'>Good Morning! {user?.name}</h2>
+            <p className='text-xs md:text-[13px] text-gray-400 mt-1.5'>
+              {moment().format("dddd Do MMM YYYY")}
+            </p>
+          </div>
+        </div>
+
+        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5'>
+          <InfoCard
+            label="Total Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.All || 0
+            )}
+            color="bg-primary"
+          />
+          <InfoCard
+            label="Pending Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.Pending || 0
+            )}
+            color="bg-violet-500"
+          />
+          <InfoCard
+            label="In Progress Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.InProgress || 0
+            )}
+            color="bg-cyan-500"
+          />
+          <InfoCard
+            label="Completed Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.Completed || 0
+            )}
+            color="bg-lime-500"
+          />
+
+        </div>
+      </div>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6'>
+        <div className='md:col-span-2'>
+          <div className='card'>
+            <div className='flex items-center justify-between'>
+              <h5 className='text-lg'>Recent Tasks</h5>
+
+              <button className='card-btn' onClick={onSeeMore}>
+                See All <LuArrowRight className='text-base' />
+              </button>
+            </div>
+
+            <TaskListTable tableData={dashboardData?.recentTasks || []} />
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
   )
 };
 
