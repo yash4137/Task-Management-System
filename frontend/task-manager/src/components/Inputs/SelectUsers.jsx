d863806b0894823961824557902b4c883cbbbba5
yash4137 <23cs011@charusat.edu.in>
2025-08-22 21:53:07 +0530

    4.0

--- frontend/task-manager/src/components/Inputs/SelectUsers.jsx
+++ frontend/task-manager/src/components/Inputs/SelectUsers.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { API_PATHS } from '../../utils/apiPaths';
 import axiosInstance from '../../utils/axiosInstance';
+import { LuUsers } from "react-icons/lu";
+import Modal from '../Modal';
+import AvatarGroup from '../AvatarGroup';
 
-const SelectUsers = ({SelectUsers,setSelectedUsers}) => {
+const SelectUsers = ({selectedUsers,setSelectedUsers}) => {
   const [allUsers, setAllUsers] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [tempSelectedUsers, setTempSelectedUsers] = useState([]);
@@ -49,8 +52,65 @@ const SelectUsers = ({SelectUsers,setSelectedUsers}) => {
     }, [selectedUsers]);
 
   return (
-    <div>SelectUsers</div>
-  )
-}
+    <div className='space-y-4 mt-2'>
+      {selectedUserAvatars.length === 0 && (
+        <button className='card-btn' onClick={() => setIsModalOpen(true)}>
+          <LuUsers className='text-sm' /> Add Members
+        </button>
+      )}
 
-export default SelectUsers
+      {selectedUserAvatars.length > 0 && (
+        <div className='cursor pointer' onClick={() => setIsModalOpen(true)}>
+          <AvatarGroup avatars={selectedUserAvatars} maxVisible={3} />
+        </div>
+      )}
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Select Users"
+      >
+        <div className='space-y-4 h-[60vh] overflow-y-auto'>
+          {allUsers.map((user) => (
+            <div
+              key={user._id}
+              className='flex items-center gap-4 p-3 border-b border-gray-200'
+            >
+              <img
+                src={user.profileImageUrl}
+                alt={user.name}
+                className='w-10 h-10 rounded-full'
+              />
+              <div className='flex-1'>
+                <p className='font-medium text-gray-800 dark:text-white'>
+                  {user.name}
+                </p>
+                <p className='text=[13px] text-gray-500'>
+                  {user.email}
+                </p>
+              </div>
+              
+              <input 
+                type='checkbox'
+                checked={tempSelectedUsers.includes(user._id)}
+                onChange={() => toggleUserSelection(user._id)}
+                className='w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded-sm outline-none'
+              />
+            </div>
+          ))}
+        </div>
+
+          <div className='flex justify-end gap-4 pt-4'>
+            <button className='card-btn' onClick={() => setIsModalOpen(false)}>
+              CANCEL
+            </button>
+            <button className='card-btn-fill' onClick={handleAssign}>
+              DONE
+            </button>
+          </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default SelectUsers;
--- frontend/task-manager/src/pages/Admin/CreateTask.jsx
+++ frontend/task-manager/src/pages/Admin/CreateTask.jsx
@@ -9,6 +9,7 @@ import moment from 'moment';
 import { LuTrash2 } from 'react-icons/lu';
 import SelectDropdown from '../../components/Inputs/SelectDropdown';
 import SelectUsers from '../../components/Inputs/SelectUsers';
+import TodoListInput from '../../components/Inputs/TodoListInput';
 
 const CreateTask = () => {
 
@@ -152,12 +153,23 @@ const CreateTask = () => {
                   </label>
 
                   <SelectUsers
-                    SelectUsers={taskData.assignedTo}
+                    selectedUsers={taskData.assignedTo}
                     setSelectedUsers={(value) => {
                       handleValueChange("assignedTo",value);
                     }}
                   />
                 </div>
+            </div>
+
+            <div className='mt-3'>
+              <label className='text-xs font-medium text-slate-600'>
+                TODO Checklist
+              </label>
+
+              <TodoListInput
+                todoList={taskData?.todoChecklist}
+                setTodoList={(value) => handleValueChange("todoChecklist", value)}
+              />
             </div>
           </div>
         </div>
