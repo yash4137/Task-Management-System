7b74cf5ecf139d91282e6637e745673196f22bcf
yash4137 <23cs011@charusat.edu.in>
2025-11-13 13:42:27 +0530

    frontend setup ready for deploy

--- frontend/task-manager/src/utils/apiPaths.js
+++ frontend/task-manager/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const API_PATHS = {
   AUTH:{
